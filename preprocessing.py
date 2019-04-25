436d42c7cb6e9c42add5751cec2de3d7a499c3e1
Dávid Pavelka <d.pavelka1@gmail.com>
2019-04-25 20:12:19 +0200

    One Hot Encoding with pandas dummies

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+import datetime
 
 data = pd.read_csv('data/train.tsv', sep="\t")
 data.set_index('train_id', inplace=True)
 # TRAIN_SIZE = 100000
 # data = data.iloc[:TRAIN_SIZE]
 
-data.to_csv("train.csv.gz", compression="gzip")
+# data.to_csv("train.csv.gz", compression="gzip")
 
 # Separate categories to custom columns
 category_path_max = 3
@@ -23,5 +24,39 @@ mask = pd.DataFrame(vc[vc])
 data.loc[data["brand_name"].isin(mask.index), "brand_name"] = "Other"
 data["name_and_brand"] = data["name"].map(str) + ' ' + data["brand_name"].map(str)
 data = data.drop("name", axis=1)
+
+before = datetime.datetime.now()
+print(f"ohe start - {before}")
+
+# item_condition_id
+encoded_item_condition_id = pd.get_dummies(data["item_condition_id"])
+data = pd.concat([data, encoded_item_condition_id], axis=1)
+data = data.drop("item_condition_id", axis=1)
+
+# brand_name
+encoded_brand_name = pd.get_dummies(data["brand_name"])
+data = pd.concat([data, encoded_brand_name], axis=1)
+data = data.drop("brand_name", axis=1)
+
+# 0_category
+encoded_category_0 = pd.get_dummies(data["0_category"])
+data = pd.concat([data, encoded_category_0], axis=1)
+data = data.drop("0_category", axis=1)
+
+# 1_category
+encoded_category_1 = pd.get_dummies(data["1_category"])
+data = pd.concat([data, encoded_category_1], axis=1)
+data = data.drop("1_category", axis=1)
+
+# 2_category
+encoded_category_2 = pd.get_dummies(data["2_category"])
+data = pd.concat([data, encoded_category_2], axis=1)
+data = data.drop("2_category", axis=1)
+
+after = datetime.datetime.now()
+print(f"ohe finish - {after}")
+print(f"took - {after - before}")
+
+data.to_csv('data/train_encoded.tsv', sep="\t")
 
 print(data.head())
